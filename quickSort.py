9368795f3dbdcc5f64cf470bacd40de55980fce0
KieceDonc <valentinverst.developer@gmail.com>
2020-10-02 09:54:59 +0200

    update

---
+++ quickSort.py
@@ -0,0 +1,26 @@
+﻿def swap(t,a,b):
+    n=0
+    n=t[a]
+    t[a]=t[b]
+    t[b]=n
+
+def partition(t, start, end):
+    pIndex=end
+    for i in range(start,end):
+        if t[i]<=t[pIndex]:
+            swap(t,i,pIndex)
+            pIndex=i
+        swap(t,pIndex,end)
+    return pIndex
+
+def quickSort(t,start,end):
+    if(start>=end) : 
+        return
+    pInd = partition(t,start,end)
+    quickSort(t,start,pInd)
+    quickSort(t,pInd+1,end)
+
+
+tab=[4,7,45,12,3,1]
+partition(tab,0,5)
+print(tab)
